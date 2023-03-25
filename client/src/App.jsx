d811b01457afd9ad59f76a8f5a34329827876d91
Pablo Cigoy <pablocigoy@gmail.com>
2023-03-25 11:44:15 +0100

    cleaning code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import React, { useEffect, useState } from "react"
-import axios from "axios"
+import {Routes, Route} from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
@@ -11,61 +9,43 @@ import Navbar from "./components/Navbar/Navbar";
 import IsPrivate from "./components/IsPrivate/IsPrivate";
 import IsAnon from "./components/IsAnon/IsAnon";
 
-const API_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:5005"
-
 function App() {
+    return (
+        <div className="App">
+            <Navbar />
 
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/word" element={<Words />} />
 
-    // const [data, setData] = useState([])
+                <Route
+                    path="/profile"
+                    element={
+                        <IsPrivate>
+                            <ProfilePage />
+                        </IsPrivate>
+                    }
+                />
 
-    // const refresh = () => {
-    //     axios
-    //         .get(`${API_URL}/pages/Words/Words`)
-    //         .then((response) => setData(response.data), console.log(data))
-    // }
-    // useEffect(() => {
-    //     refresh()
-    // }, [])
-    // console.log(data)
-
-
-
-  return (
-    <div className="App">
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/word" element={<Words  />} />
-
-        <Route
-          path="/profile"
-          element={
-            <IsPrivate>
-              <ProfilePage />
-            </IsPrivate>
-          }
-        />
-
-        <Route
-          path="/signup"
-          element={
-            <IsAnon>
-              <SignupPage />
-            </IsAnon>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <IsAnon>
-              <LoginPage />
-            </IsAnon>
-          }
-        />
-      </Routes>
-    </div>
-  );
+                <Route
+                    path="/signup"
+                    element={
+                        <IsAnon>
+                            <SignupPage />
+                        </IsAnon>
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={
+                        <IsAnon>
+                            <LoginPage />
+                        </IsAnon>
+                    }
+                />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
