bb19eb0e2cb936db702a91bce33c968dd9187881
antoniosaleme <kilo@disroot.org>
2023-04-02 23:55:43 +0200

    test: testing eslint

--- client/src/components/Topic/TopicDetails.jsx
+++ client/src/components/Topic/TopicDetails.jsx
@@ -3,10 +3,10 @@ import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import { AuthContext } from '../../context/auth.context';
 import PostCard from '../Blog/PostCard';
-import CreateAPost from "../Blog/CreateAPost";
+import CreateAPost from '../Blog/CreateAPost';
 
 export default function TopicDetails() {
-  const params = useParams()
+  const params = useParams();
   const id = params.id;
 
   const [topic, setTopic] = useState('');
@@ -15,20 +15,19 @@ export default function TopicDetails() {
   console.log(posts);
 
   useEffect(() => {
-    axios.get(`/api/topic/details/${id}`)
-        .then(response => {
-            setTopic(response?.data?.topic);
-            setPosts(response?.data?.topic?.posts)
-        })
-  }, [])
+    axios.get(`/api/topic/details/${id}`).then((response) => {
+      setTopic(response?.data?.topic);
+      setPosts(response?.data?.topic?.posts);
+    });
+  }, []);
 
   return (
     <>
-     <h1>{topic.title}</h1>
-     {posts.map(post => (
-       <PostCard key={post._id} post={post} />
+      <h1>{topic.title}</h1>
+      {posts.map((post) => (
+        <PostCard key={post._id} post={post} />
       ))}
-     <CreateAPost setPosts={setPosts} posts={posts} />
+      <CreateAPost setPosts={setPosts} posts={posts} />
     </>
-  )
+  );
 }
