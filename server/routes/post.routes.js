14725244d92912990ba8a53f35254c8c96fa498d
PABLO <80485682+Pablo-Mdz@users.noreply.github.com>
2023-04-07 12:34:54 +0200

    edit and delete topic working, also the created by: ... (#51)
    * bug fixed with auth edit button
    
    * edit title working
    
    * 
    
    * last changes
    
    * auth.routes"
    
    * all routes

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -3,6 +3,7 @@ const Post = require('../models/Post');
 const Topic = require('../models/Topic');
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 const User = require('../models/User');
+const { findById } = require("../models/Post");
 
 router.post("/", isAuthenticated, (req, res) => {
     console.log('this is req.body of post:', req.body)
@@ -14,49 +15,51 @@ router.post("/", isAuthenticated, (req, res) => {
         body,
         author: authorId
     })
-      .then(newPost => {
-        Topic.findByIdAndUpdate(topicId, {$push: { posts: newPost._id}})
-          .then(updatedTopic => {
-            User.findByIdAndUpdate( authorId, {$push: { posts: newPost._id}})
-            .then(updatedUser => {
-              Post.findById(newPost._id)
-                .populate("author")
-                .then(populatedPost => {
-                  res.json(populatedPost);
+
+        .then(newPost => {
+            Topic.findByIdAndUpdate(topicId, { $push: { posts: newPost._id } })
+                .then(updatedTopic => {
+                    User.findByIdAndUpdate(authorId, { $push: { posts: newPost._id } })
+                        .then(updatedUser => {
+                            Post.findById(newPost._id)
+                                .populate("author")
+                                .then(populatedPost => {
+                                    res.json(populatedPost);
+                                })
+                        })
                 })
-            })
-          })
-          .catch(err => console.log(err));
-      })
+                .catch(err => console.log(err));
+        })
 });
 
 router.get("/:postId", (req, res) => {
-  const postId = req.params.postId;
-  Post.findById(postId)
-    .populate("author")
-    .then(post => {
-      res.status(200).json(post);
-    })
-    .catch(err => console.log(err));
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .populate("author")
+        .then(post => {
+            res.status(200).json(post);
+        })
+        .catch(err => console.log(err));
 });
 
 
 router.put("/:postId", (req, res) => {
-  const { title, body } = req.body;
-  Post.findByIdAndUpdate(req.params.postId, { title, body }, { new: true })
-    .then(updatedPost => {
-      res.status(200).json(updatedPost);
-    })
-    .catch(err => console.log(err));
+    const { title, body } = req.body;
+    Post.findByIdAndUpdate(req.params.postId, { title, body }, { new: true })
+        .then(updatedPost => {
+            res.status(200).json(updatedPost);
+        })
+        .catch(err => console.log(err));
 });
 
 router.delete("/:postId", (req, res) => {
-  const postID = req.params.postId;
-  Post.findByIdAndDelete(postID)
-    .then(() => {
-      res.status(200).json({message: 'Post deleted'})
-    })
-    .catch(err => console.log(err));
+    const postID = req.params.postId;
+    Post.findByIdAndDelete(postID)
+        .then(() => {
+            res.status(200).json({ message: 'Post deleted' })
+        })
+        .catch(err => console.log(err));
 })
+
 
 module.exports = router;
--- server/routes/topic.routes.js
+++ server/routes/topic.routes.js
@@ -10,39 +10,64 @@ router.post("/", isAuthenticated, (req, res) => {
         title: title,
         author: authorId
     })
-      .then(newTopic => {
-        User.findByIdAndUpdate( authorId, {$push: { topics: newTopic._id}})
-          .then(updatedUser => {
-            Topic.findById(newTopic._id)
-                .populate("author")
-                .then(populatedTopic => {
-                  res.json(populatedTopic);
-                  console.log("populated topic: ", populatedTopic)
+        .then(newTopic => {
+            User.findByIdAndUpdate(authorId, { $push: { topics: newTopic._id } })
+                .then(updatedUser => {
+                    Topic.findById(newTopic._id)
+                        .populate("author")
+                        .then(populatedTopic => {
+                            res.json(populatedTopic);
+                            console.log("populated topic: ", populatedTopic)
+                        })
                 })
-          })
-          .catch(err => console.log(err));
-      })
+                .catch(err => console.log(err));
+        })
 });
 
 router.get("/details/:id", (req, res) => {
-  const topicId = req.params.id;
-  Topic.findById(topicId) 
-    .populate({
-      path: "posts",
-    })
-    .then(topicFromDB => {
-      console.log("topicFromDB: ", topicFromDB)
-      res.json({ topic: topicFromDB })
-    })
-    .catch(err => console.log(err))
+    const topicId = req.params.id;
+    Topic.findById(topicId)
+        .populate({
+            path: "posts",
+            populate: {
+                path: "author",
+            }
+        })
+        .then(topicFromDB => {
+            // console.log("topicFromDB: ", topicFromDB)
+            res.json({ topic: topicFromDB })
+        })
+        .catch(err => console.log(err))
 });
 
 router.get("/list-topics", (req, res) => {
-  Topic.find({})
-    .then(topicsFromDB => {
-      res.json({ topics: topicsFromDB });
-    })
-    .catch(err => console.log(err));
+    Topic.find({})
+        .then(topicsFromDB => {
+            res.json({ topics: topicsFromDB });
+        })
+        .catch(err => console.log(err));
 })
+
+router.put("/details/:topicId", (req, res) => {
+    const { title } = req.body;
+    Topic.findByIdAndUpdate(req.params.topicId, { title }, { new: true })
+        .then(updatedTopic => {
+            console.log(" updated topic", updatedTopic)
+            res.status(200).json(updatedTopic);
+        })
+        .catch(err => console.log(err));
+});
+
+router.delete("/details/:topicId", (req, res) => {
+    const topicID = req.params.topicId;
+    console.log("topic id delete", topicID)
+    Topic.findByIdAndDelete(topicID)
+        .then(() => {
+            res.status(200).json({ message: 'Topic deleted' });
+        })
+        .catch(err => console.log(err));
+})
+
+
 
 module.exports = router;
