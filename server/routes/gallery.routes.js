754997400e192829e0308b0af491172ffcab3fcd
Pablo Cigoy <pablocigoy@gmail.com>
2023-04-07 14:17:54 +0200

    gallery.routesuploaded

--- server/routes/gallery.routes.js
+++ server/routes/gallery.routes.js
@@ -4,40 +4,43 @@ const Gallery = require('../models/Gallery');
 const { uploader, cloudinary } = require('../config/cloudinary');
 
 router.post('/add-photo', uploader.single('gallery'), (req, res, next) => {
-  const { imgUrl, publicId } = req.body;
-  //console.log("REQUEST BODY: ", req.body)
-  Gallery.create({
-    imgUrl,
-    publicId,
-  })
-    .then((e) => {
-      const { title, _id } = e;
-      const event = { title, _id };
-      res.status(201).json({ event });
+    const { title, imgUrl, publicId } = req.body;
+    console.log("REQUEST BODY: ", req.body)
+    Gallery.create({
+        title,
+        imgUrl,
+        publicId,
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: 'Internal Server Error' });
-    });
+        .then((e) => {
+            const { title, _id } = e;
+            const event = { title, _id };
+            res.status(201).json({ event });
+            console.log("title",e)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ message: 'Internal Server Error' });
+        });
 });
 
 router.get('/', (req, res) => {
-  Gallery.find().then((gallery) => {
-    res.status(200).json(gallery);
-  });
+    Gallery.find()
+    .then((gallery) => {
+        res.status(200).json(gallery);
+    });
 });
 
 router.post('/delete/:id', (req, res, next) => {
-  Gallery.findByIdAndDelete({ _id: req.params.id })
-    .then((data) => {
-      if (data.imgUrl) {
-        cloudinary.uploader.destroy(data.publicId);
-      }
-      res.status(200).json({ message: 'Entry deleted' });
-    })
-    .catch((err) => {
-      next(err);
-    });
+    Gallery.findByIdAndDelete({ _id: req.params.id })
+        .then((data) => {
+            if (data.imgUrl) {
+                cloudinary.uploader.destroy(data.publicId);
+            }
+            res.status(200).json({ message: 'Entry deleted' });
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
 /* router.post("/upload", uploader.single("imageURL"), (req, res, next) => {
