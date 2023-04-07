30def0a4584c9fdfb912a79d90d81e53f3618f14
PABLO <80485682+Pablo-Mdz@users.noreply.github.com>
2023-04-07 14:27:18 +0200

    Search bar and titles on pictures working. (#53)

---
+++ client/src/components/Gallery/AddPicture.jsx
@@ -0,0 +1,125 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
+export default function AddPicture() {
+    const [image, setImage] = useState('');
+    const [setErrorMessage] = useState('');
+    const [title, setTitle] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = new FormData();
+        data.append('file', image);
+        data.append('upload_preset', 'auh8nzbq');
+        data.append('cloud_name', 'be-chef');
+        fetch('https://api.cloudinary.com/v1_1/be-chef/image/upload', {
+            method: 'post',
+            body: data,
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            //console.log("DATA: ", data);
+            const storedToken = localStorage.getItem('authToken');
+            const requestBody = {
+                title: title,
+                publicId: data.public_id,
+                imgUrl: data.url,
+                };
+                if (data.url.length > 1) {
+                    axios.post(`/api/gallery/add-photo/`, requestBody, {headers: { Authorization: `Bearer ${storedToken}` }})
+                    .then((response) => {
+                            
+                            navigate('/gallery');
+                            console.log("RESPONSE: ", response);
+                            setTitle('');
+                        })
+                        .catch((err) => {
+                            const errorDescription = err.response.data.message;
+                            setErrorMessage(errorDescription);
+                        });
+                }
+            })
+            .catch((err) => {
+                const errorDescription = err.response.data.message;
+                setErrorMessage(errorDescription);
+            });
+        setImage('');
+        
+    };
+
+    return (
+        <>
+            <div className="block p-6 rounded-lg shadow-lg bg-white max-w-md mx-auto mt-6">
+                <form>
+                    <div className="form-group mb-6">
+                        <div className="mb-4">
+                            <label
+                                className="block text-gray-700 font-bold mb-2"
+                                htmlFor="title"
+                            >
+                                Title:
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="title"
+                                type="text"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </div>
+                        <input
+                            type="file"
+                            className="form-control block
+                w-full
+                px-3
+                py-1.5
+                text-base
+                font-normal
+                text-gray-700
+                bg-white bg-clip-padding
+                border border-solid border-gray-300
+                rounded
+                transition
+                ease-in-out
+                m-0
+                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="exampleInput8"
+                            placeholder="Add a picture"
+                            onChange={(e) => setImage(e.target.files[0])}
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="
+            w-full
+            px-6
+            py-2.5
+            bg-gray-900
+            text-white
+            font-medium
+            text-xs
+            leading-tight
+            uppercase
+            rounded
+            shadow-md
+            hover:bg-gray-700 hover:shadow-lg
+            focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-gray-800 active:shadow-lg
+           
+            transition
+            duration-150
+            ease-in-out"
+                        onClick={handleSubmit}
+                    >
+                        Send
+                    </button>
+                </form>
+            </div>
+        </>
+    );
+}
--- client/src/pages/GalleryPage.jsx
+++ client/src/pages/GalleryPage.jsx
@@ -7,6 +7,7 @@ import DeletePicture from '../components/Gallery/DeletePicture';
 export default function GalleryPage() {
   const { user, isLoggedIn } = useContext(AuthContext);
   const [gallery, setGallery] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios
@@ -17,37 +18,57 @@ export default function GalleryPage() {
       .catch((err) => console.log(err));
   }, []);
 
+  const filtered = gallery.filter((oneData) => {
+    if (!oneData.title) {
+      return false;
+    } else {
+      return (
+        oneData.title &&
+        oneData.title.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+  });
+
   return (
     <>
       {gallery ? (
         <section className="overflow-hidden text-gray-700 mb-32">
           <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
+            <input
+              placeholder="Search by name or translation"
+              type="text"
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
             <div className="flex flex-wrap -m-1 md:-m-2">
-              {gallery.map((picture) => (
-                <div className="flex flex-wrap w-1/3" key={picture._id}>
-                  <div className="w-full p-1 md:p-2">
-                    <img
-                      alt="gallery"
-                      className="block object-cover object-center w-full h-full rounded-lg hover:shadow-lg transition duration-300 ease-in-out"
-                      src={picture.imgUrl}
-                    />
+              {filtered &&
+                filtered.map((picture) => (
+                  <div className="flex flex-wrap w-1/3" key={picture._id}>
+                    <div className="w-full p-1 md:p-2">
+                      <p>{picture.title}</p>
+                      <img
+                        alt="gallery"
+                        className="block object-cover object-center w-full h-full rounded-lg hover:shadow-lg transition duration-300 ease-in-out"
+                        src={picture.imgUrl}
+                      />
+                    </div>
+                    {isLoggedIn && <DeletePicture id={picture._id} />}
                   </div>
-                  {isLoggedIn && 
-                    <DeletePicture id={picture._id} />
-                  }
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         </section>
       ) : null}
-        {isLoggedIn && 
-         <div className="flex justify-center">
+      {isLoggedIn && (
+        <div className="flex justify-center">
           <div className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 w-32 border border-gray-400 rounded shadow flex justify-center mb-36">
-           <Link to="/gallery/add-picture">Add picture</Link>
+            <Link to="/gallery/add-picture">Add picture</Link>
           </div>
-         </div>
-       }
+        </div>
+      )}
     </>
   );
 }
