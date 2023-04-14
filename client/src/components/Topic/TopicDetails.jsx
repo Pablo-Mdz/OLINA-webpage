fdba72cda3aa0d9897a7f879205c51a4b8bf287f
Pablo Cigoy <pablocigoy@gmail.com>
2023-04-14 17:16:21 +0200

    create a post button with smooth movment and all the post on topics component

--- client/src/components/Topic/TopicDetails.jsx
+++ client/src/components/Topic/TopicDetails.jsx
@@ -1,15 +1,16 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useRef, useLayoutEffect } from 'react';
 import axios from 'axios';
 import PostCard from '../Blog/PostCard';
 import CreateAPost from '../Blog/CreateAPost';
 import { AuthContext } from '../../context/auth.context';
 
-export default function TopicDetails({ id }) {
+export default function TopicDetails({ id, selectedTopicId }) {
   const { isLoggedIn } = useContext(AuthContext);
   const [topic, setTopic] = useState('');
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState('');
-
+  const [isCreating, setIsCreating] = useState(false);
+  const createPostRef = useRef(null);
   const filteredPost = posts.filter(
     (post) =>
       post.body.toLowerCase().includes(search.toLowerCase()) ||
@@ -19,23 +20,54 @@ export default function TopicDetails({ id }) {
   const postSortedByDate = filteredPost.sort(
     (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
   );
-
+  console.log(posts);
   useEffect(() => {
-    axios
-      .get(`/api/topic/details/${id}`)
-      .then((response) => {
-        setTopic(response?.data?.topic);
-        setPosts(response?.data?.topic?.posts);
-      })
-      .catch((err) => console.log(err));
+    if (id === 'all') {
+      axios
+        .get('/api/post')
+        .then((response) => {
+          setPosts(response.data.posts);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      axios
+        .get(`/api/topic/details/${id}`)
+        .then((response) => {
+          setTopic(response?.data?.topic);
+          setPosts(response?.data?.topic?.posts);
+        })
+        .catch((err) => console.log(err));
+    }
   }, [id, search]);
+
+  const handleCreatePost = () => {
+    setIsCreating(!isCreating);
+    
+  };
+
+  useLayoutEffect(() => {
+    if (isCreating && createPostRef.current) {
+      createPostRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [isCreating]);
+  
 
   return (
     <div className="topic-details-container w-full px-4">
-      <h1 className="text-4xl font-bold text-gray-800 mb-8 font-pop mt-6">
-        TOPIC: {topic.title}
-      </h1>
+      <div className="flex justify-between items-center my-2">
+      <h1 className="text-4xl font-bold text-gray-800 mb-8 font-pop mt-6 w-full text-center">
+          {!topic ? "ALL POSTS" :  "TOPIC:  " + topic.title}
+        </h1>
 
+        {isLoggedIn && selectedTopicId !== 'all' && (
+          <button
+            onClick={handleCreatePost}
+            className="bg-cyan-500 text-white font-medium px-6 my-4 p-2 rounded mt-2   top-4 right-4 z-10 whitespace-nowrap"
+          >
+            {!isCreating ? 'Create a Post' : 'hidde menu'}
+          </button>
+        )}
+      </div>
       <label
         htmlFor="description"
         className="block text-gray-700 font-semibold mb-2"
@@ -56,8 +88,10 @@ export default function TopicDetails({ id }) {
         ))}
       </div>
       <div className="">
-        {isLoggedIn && (
-          <CreateAPost id={id} setPosts={setPosts} posts={posts} />
+        {isCreating && (
+          <div ref={createPostRef}>
+            <CreateAPost id={id} setPosts={setPosts} posts={posts} />
+          </div>
         )}
       </div>
     </div>
