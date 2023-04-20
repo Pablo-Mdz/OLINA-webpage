a3de011b24afd52633df64d49b446ed5d31c44fd
Pablo Cigoy <pablocigoy@gmail.com>
2023-04-20 22:35:31 +0200

    page Not Found  done

--- client/src/pages/NotFoundPage/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -5,13 +5,13 @@ function NotFoundPage() {
     <div className="h-screen flex font-pop">
       <div className="w-1/4 bg-white flex flex-col justify-center items-center p-8 my-10">
         <h1 className="text-6xl font-bold text-gray-900 mb-4">Uh-oh!</h1>
-        <p className="text-3xl text-gray-700 mb-6 mx-10">
+        <p className="text-4xl text-gray-700 mb-9 leading-10 mx-10">
           Error 404. Like falling off a seesaw, things don’t go as planned and
           we can’t you connect to this page.
         </p>
         <a
           href="/"
-          className="inline-block bg-purple-400  rounded-full px-3 py-1 text-xl font-semibold text-white hover:bg-gray-700 mr-2 mb-2  hover:text-white"
+          className="inline-block bg-purple-400  rounded-full px-3 py-1 text-3xl font-semibold text-white hover:bg-gray-700 mr-2 mb-2  hover:text-white"
         >
           Return Home
         </a>
