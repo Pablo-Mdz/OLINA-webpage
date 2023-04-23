f32e5d6a8103552a85f8c96ae0c36c814e15d082
Deniz Saganak <100470456+YuruBeDeniz@users.noreply.github.com>
2023-04-23 17:43:39 +0200

    use react-quill to create about me text (#73)

--- client/src/components/AboutMe/CreateAboutMe.js
+++ client/src/components/AboutMe/CreateAboutMe.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export default function CreateAboutMe() {
   const [aboutMe, setAboutMe] = useState();
@@ -10,10 +12,14 @@ export default function CreateAboutMe() {
     const requestBody = { aboutMe };
     axios.post(`/api/about-me`, requestBody)
         .then(response => {
-           window.location.reload(false);
+          window.location.reload(false);
         })
         .catch((err) => console.log(err));
-  }
+  };
+
+  const handleEditorChange = (content) => {
+    setAboutMe(content);
+  };
 
   return (
     <div>
@@ -23,14 +29,16 @@ export default function CreateAboutMe() {
             <form 
                 onSubmit={handleSubmit}
                 className="mb-8">
-             <textarea 
+             <ReactQuill 
+                theme="snow" 
                 value={aboutMe}
-                onChange={(e) => setAboutMe(e.target.value)}
-                className="h-80 w-full border border-gray-400 py-2 px-3 rounded-md text-gray-700 leading-tight focus:outline-none focus:border-purple-600"
+                onChange={handleEditorChange}
+                className="h-80 w-full py-2 px-3 rounded-md text-gray-700 leading-tight focus:outline-none focus:border-purple-600"
              />
              <br />
-             <button
-              className='px-4 py-1 border-2 border-gray-900 rounded uppercase font-medium text-xs'>Add About Me</button>
+             <button className='px-4 py-1 mt-6 border-2 border-gray-900 rounded uppercase font-medium text-xs'>
+               Add About Me
+             </button>
             </form>
           </div>
         </div>
