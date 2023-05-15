c953b55fb03484acc06188a88a009fe5010bb6b8
antoniosaleme <kilo@disroot.org>
2023-05-15 16:41:49 -0300

    deploy main in turisnoa.com

--- app.js
+++ app.js
@@ -39,7 +39,13 @@ app.use("/api/about-me", aboutMe);
 const word = require("./routes/word.routes");
 app.use("/", word);
 // console.log('test word',word)
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
 
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
