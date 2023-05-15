35e895e328979c01c7093a8cca89d6ee3ff83bef
antoniosaleme <kilo@disroot.org>
2023-05-15 22:44:43 +0200

    fix: remove build stuff

--- app.js
+++ app.js
@@ -44,8 +44,7 @@ app.use('/api/about-me', aboutMe);
 
 const word = require('./routes/word.routes');
 app.use('/', word);
-// console.log('test word',word)
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+
 require('./error-handling')(app);
 
 module.exports = app;
