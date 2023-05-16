f88e8d78a03a7a05cb8bf1b8f91787d3bb9c0efe
Pablo Cigoy <pablocigoy@gmail.com>
2023-05-16 09:16:36 +0200

    second try react-quill updated

--- client/src/components/BlogPost/CreateAPost.jsx
+++ client/src/components/BlogPost/CreateAPost.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+
+import { useState } from 'react';
 import axios from 'axios';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -6,38 +7,46 @@ import 'react-quill/dist/quill.snow.css';
 export default function CreateAPost({ id }) {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
-  const [images, setImages] = useState([]);
+  const [image, setImage] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
+    const data = new FormData();
+    data.append('file', image);
+    data.append('upload_preset', 'auh8nzbq');
+    data.append('cloud_name', 'be-chef');
 
-    try {
-      const formData = new FormData();
-      formData.append('title', title);
-      formData.append('body', body);
-
-      images.forEach((image) => {
-        formData.append('images', image);
-      });
-
-      const response = await axios.post(`/api/post`, formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
-
-      console.log(response.data.newPost);
-
-      setTitle('');
-      setBody('');
-      setImages([]);
-
-      window.location.reload();
-    } catch (error) {
-      if (error.response) {
-        const errorDescription = error.response.data.message;
+    fetch('https://api.cloudinary.com/v1_1/be-chef/image/upload', {
+      method: 'post',
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const storedToken = localStorage.getItem('authToken');
+        const requestBody = {
+          title: title,
+          body: body,
+          topicId: id,
+          publicId: data.public_id,
+          imgUrl: data.url,
+        };
+        axios
+          .post(`/api/post/`, requestBody, {
+            headers: { Authorization: `Bearer ${storedToken}` },
+          })
+          .then((response) => {
+            console.log(response.data.newPost);
+            setTitle('');
+            setBody('');
+            setImage(null);
+            window.location.reload(false);
+          });
+      })
+      .catch((err) => {
+        const errorDescription = err.response.data.message;
         setErrorMessage(errorDescription);
-      }
-    }
+      });
   };
 
   const handleEditorChange = (content) => {
@@ -45,19 +54,22 @@ export default function CreateAPost({ id }) {
   };
 
   const handleImageChange = (event) => {
-    const files = event.target.files;
-    const imageArray = Array.from(files);
-    setImages(imageArray);
+    setImage(event.target.files[0]);
   };
 
   return (
     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex justify-center">
       <div style={{ width: '80%' }}>
         <br />
-        <h3 className="text-2xl font-medium mb-4">Create a new post related to this topic</h3>
+        <h3 className="text-2xl font-medium mb-4">
+          Create a new post related to this topic
+        </h3>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="title">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="title"
+            >
               Title:
             </label>
             <input
@@ -70,7 +82,10 @@ export default function CreateAPost({ id }) {
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="body">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="body"
+            >
               Body:
             </label>
             <ReactQuill
@@ -81,34 +96,37 @@ export default function CreateAPost({ id }) {
             />
           </div>
           <div className="mb-4">
-            <br />
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="image">
-              Images:
-            </label>
-            <input
+            <label
+              className="block text-gray700 font-bold mb-2"
+              htmlFor="file"
+              >
+              File:
+              </label>
+              <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="image"
+              id="file"
               type="file"
-              multiple
               onChange={handleImageChange}
               required
-            />
-          </div>
-          <div className="flex">
-            <button
+              />
+              </div>
+              <div className="flex">
+              <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline m-auto"
               type="submit"
-            >
+              >
               Add a new post
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-}
-
-
+              </button>
+              </div>
+              {errorMessage && (
+              <div className="text-red-500 mt-2 text-sm">
+              {errorMessage}
+              </div>
+              )}
+              </form>
+              </div>
+              </div>
+              );
 
 
 // orignial
@@ -171,6 +189,9 @@ export default function CreateAPost({ id }) {
 
 //   return (
 //     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex justify-center">
+
+}
+
 //       <div style={{ width: '80%' }}>
 //         <br />
 //         <h3 className="text-2xl font-medium mb-4">
