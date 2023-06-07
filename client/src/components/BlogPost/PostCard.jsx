45a5cc3155ca690b0cfd62cf1381f49377184be5
antoniosaleme <kilo@disroot.org>
2023-06-07 22:50:36 +0200

    chore: clean code

--- client/src/components/BlogPost/PostCard.jsx
+++ client/src/components/BlogPost/PostCard.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ReadingTime } from '../Words/ReadingTime';
 
 export default function PostCard({ post, RTime }) {
-  //   const { isLoggedIn, user } = useContext(AuthContext);
 
   const date = new Date(post.createdAt).toLocaleString();
 
@@ -14,52 +13,49 @@ export default function PostCard({ post, RTime }) {
       (words.length > maxLength ? '...' : '')
     );
   };
-console.log(post.comments)
   return (
-    <>
-      <div className="w-full h-96 rounded overflow-hidden shadow-lg bg-opacity-25 relative flex">
-        <div className="w-1/2 relative z-10">
-          <div className="px-6 py-4">
-            <h1 className="text-lg mb-1">{post.title}</h1>
-          </div>
-          <div
-            className="align-start mx-2 my-1 overflow-hidden"
-            style={{
-              maxHeight: 'calc(100% - 8rem)',
-            }}
-          >
-            <div dangerouslySetInnerHTML={{ __html: postBodyPreview() }}></div>
-          </div>
-          <footer className="absolute bottom-0 left-0 right-0 flex items-center justify-between p-3">
-            <div className="text-sm text-left">
-              <ReadingTime text={post.body} />
-              <p className="text-gray-600 my-2">
-                Date: {new Date(date).toLocaleDateString('es-ES')}
-              </p>
-              <p className="text-gray-900 font-medium">
-                Created by: {post.author.name}
-              </p>
-              <p>{post.likes} ❤️ </p>
-              <p>{post.comments.length} Comments</p>
-            </div>
-          </footer>
+    <div className="w-full h-96 rounded overflow-hidden shadow-lg bg-opacity-25 relative flex">
+      <div className="w-1/2 relative z-10">
+        <div className="px-6 py-4">
+          <h1 className="text-lg mb-1">{post.title}</h1>
         </div>
         <div
-          className="w-1/2 h-full bg-cover bg-no-repeat bg-center bg-opacity-75 relative"
+          className="align-start mx-2 my-1 overflow-hidden"
           style={{
-            backgroundImage: `url(${
-              post.imgUrl ? post.imgUrl : 'https://picsum.photos/500/300'
-            })`,
+            maxHeight: 'calc(100% - 8rem)',
           }}
         >
-          <a
-            href={`/post/${post._id}`}
-            className="bg-blue-500 text-white font-medium px-2 py-1 my-1 rounded-full mt-2 absolute bottom-3 right-3"
-          >
-            Read more
-          </a>
+          <div dangerouslySetInnerHTML={{ __html: postBodyPreview() }}></div>
         </div>
+        <footer className="absolute bottom-0 left-0 right-0 flex items-center justify-between p-3">
+          <div className="text-sm text-left">
+            <ReadingTime text={post.body} />
+            <p className="text-gray-600 my-2">
+              Date: {new Date(date).toLocaleDateString('es-ES')}
+            </p>
+            <p className="text-gray-900 font-medium">
+              Created by: {post.author.name}
+            </p>
+            <p>{post.likes} ❤️ </p>
+            <p>{post.comments.length} Comments</p>
+          </div>
+        </footer>
       </div>
-    </>
+      <div
+        className="w-1/2 h-full bg-cover bg-no-repeat bg-center bg-opacity-75 relative"
+        style={{
+          backgroundImage: `url(${
+            post.imgUrl ? post.imgUrl : 'https://picsum.photos/500/300'
+          })`,
+        }}
+      >
+        <a
+          href={`/post/${post._id}`}
+          className="bg-blue-500 text-white font-medium px-2 py-1 my-1 rounded-full mt-2 absolute bottom-3 right-3"
+        >
+          Read more
+        </a>
+      </div>
+    </div>
   );
 }
