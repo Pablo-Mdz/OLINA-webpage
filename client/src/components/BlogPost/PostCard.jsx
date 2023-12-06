2a09a9ab59ae6bf989169acb2163455b62b1cada
antoniosaleme <77642907+antoniosaleme@users.noreply.github.com>
2023-12-06 20:12:50 +0100

    Update PostCard.jsx

--- client/src/components/BlogPost/PostCard.jsx
+++ client/src/components/BlogPost/PostCard.jsx
@@ -21,6 +21,7 @@ export default function PostCard({ post, RTime }) {
     const maxLength = 5;
     return words.slice(0, maxLength).join(' ') + (words.length > maxLength ? '...' : '');
   };
+  
 
   return (
     <>
@@ -64,35 +65,7 @@ export default function PostCard({ post, RTime }) {
             </footer>
           </div>
         </div>
-        <footer className="absolute bottom-0 left-0 right-0 flex items-center justify-between p-3">
-          <div className="text-sm text-left">
-            <ReadingTime text={post.body} />
-            <p className="text-gray-600 my-2">
-              Date: {new Date(date).toLocaleDateString('es-ES')}
-            </p>
-            <p className="text-gray-900 font-medium">
-              Created by: {post.author.name}
-            </p>
-            <p>{post.likes} ❤️ </p>
-            <p>{post.comments.length} Comments</p>
-          </div>
-        </footer>
       </div>
-      <div
-        className="w-1/2 h-full bg-cover bg-no-repeat bg-center bg-opacity-75 relative"
-        style={{
-          backgroundImage: `url(${
-            post.imgUrl ? post.imgUrl : 'https://picsum.photos/500/300'
-          })`,
-        }}
-      >
-        <a
-          href={`/post/${post._id}`}
-          className="bg-blue-500 text-white font-medium px-2 py-1 my-1 rounded-full mt-2 absolute bottom-3 right-3"
-        >
-          Read more
-        </a>
-      </div>
-    </div>
+    </>
   );
 }
