99353d0c80d5a0046dd488a6863ec45c3b8563a8
PABLO <80485682+Pablo-Mdz@users.noreply.github.com>
2023-12-06 19:14:19 +0100

    Update PostCard Component Layout (#87)

--- client/src/components/BlogPost/PostCard.jsx
+++ client/src/components/BlogPost/PostCard.jsx
@@ -1,63 +1,69 @@
 import React from 'react';
-import { ReadingTime } from '../Words/ReadingTime';
+// import { ReadingTime } from '../Words/ReadingTime';
 
 export default function PostCard({ post, RTime }) {
-  //   const { isLoggedIn, user } = useContext(AuthContext);
-
   const date = new Date(post.createdAt).toLocaleString();
 
   const postBodyPreview = () => {
-    const words = post.body.split(' ');
-    const maxLength = 12;
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(post.body, 'text/html');
+    const textContent = doc.body.textContent || '';
+    const words = textContent.split(' ');
+    const maxLength = 3;
     return (
       words.slice(0, maxLength).join(' ') +
       (words.length > maxLength ? '...' : '')
     );
+};
+
+  const postTitlePreview = () => {
+    const words = post.title.split(' ');
+    const maxLength = 5;
+    return words.slice(0, maxLength).join(' ') + (words.length > maxLength ? '...' : '');
   };
-console.log(post.comments)
+  
+
   return (
     <>
-      <div className="w-full h-96 rounded overflow-hidden shadow-lg bg-opacity-25 relative flex">
-        <div className="w-1/2 relative z-10">
-          <div className="px-6 py-4">
-            <h1 className="text-lg mb-1">{post.title}</h1>
-          </div>
+      <div className="flex flex-wrap mx-5 ">
+        <div className=" w-96 h-96 rounded overflow-hidden shadow-lg bg-opacity-25 flex flex-col ">
           <div
-            className="align-start mx-2 my-1 overflow-hidden"
+            className="bg-cover bg-center"
             style={{
-              maxHeight: 'calc(100% - 8rem)',
+              backgroundImage: `url(${
+                post.imgUrl ? post.imgUrl : 'https://picsum.photos/500/300'
+              })`,
+              height: '55%',
             }}
-          >
-            <div dangerouslySetInnerHTML={{ __html: postBodyPreview() }}></div>
+          ></div>
+
+          <div className="bg-white  px-2 flex flex-col " style={{ height: '45%' }}>
+            <h1 className="text-2xl font-bold mb-1 flex ml-4">{postTitlePreview()}</h1>
+            <p
+              className="text-sm text-gray-600 my-2 flex ml-4 "
+              style={{ maxHeight: '3rem' }}
+            >
+              {postBodyPreview()}
+            </p>
+
+            <footer className="flex justify-between items-end  py-5 ">
+              <div className="text-gray-600 text-xs  ">
+                <p className="flex justify-start ">{post.likes} ❤️</p>
+                <p>{new Date(date).toLocaleDateString('es-ES')}</p>
+              </div>
+              <div className="flex justify-start">
+                <div className="text-gray-600 text-xs ">
+                  <p>{post.comments.length} Comentarios</p>
+                </div>
+              </div>
+              <a
+                href={`/post/${post._id}`}
+                className="bg-blue-800 text-white font-medium px-2 py-1 mb-3 rounded-lg no-underline hover:bg-blue-700"
+              >
+                read more
+              </a>
+            </footer>
           </div>
-          <footer className="absolute bottom-0 left-0 right-0 flex items-center justify-between p-3">
-            <div className="text-sm text-left">
-              <ReadingTime text={post.body} />
-              <p className="text-gray-600 my-2">
-                Date: {new Date(date).toLocaleDateString('es-ES')}
-              </p>
-              <p className="text-gray-900 font-medium">
-                Created by: {post.author.name}
-              </p>
-              <p>{post.likes} ❤️ </p>
-              <p>{post.comments.length} Comments</p>
-            </div>
-          </footer>
-        </div>
-        <div
-          className="w-1/2 h-full bg-cover bg-no-repeat bg-center bg-opacity-75 relative"
-          style={{
-            backgroundImage: `url(${
-              post.imgUrl ? post.imgUrl : 'https://picsum.photos/500/300'
-            })`,
-          }}
-        >
-          <a
-            href={`/post/${post._id}`}
-            className="bg-blue-500 text-white font-medium px-2 py-1 my-1 rounded-full mt-2 absolute bottom-3 right-3"
-          >
-            Read more
-          </a>
         </div>
       </div>
     </>
