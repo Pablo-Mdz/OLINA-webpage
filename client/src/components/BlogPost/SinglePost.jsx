e778a9d3dbf86c09500ede0b5b5d26850f1653ff
antoniosaleme <kilo@disroot.org>
2024-05-15 17:54:24 +0200

    refactor: wip

--- client/src/components/BlogPost/SinglePost.jsx
+++ client/src/components/BlogPost/SinglePost.jsx
@@ -84,116 +84,38 @@ export const SinglePost = () => {
   };
 
   return (
-    <>
-      <div
-        ref={componentRef}
-        className="flex justify-start items-center relative bg-gray-200 pt-5 pb-10 font-pop"
-      >
-        <div className="flex flex-col  w-full h-full">
-          <div className="self-start text-4xl font-bold capitalize">
-            <h1>{post?.title}</h1>
-          </div>
-          <div
-            dangerouslySetInnerHTML={{ __html: post?.body }}
-            className="post-content self-start "
-          ></div>
-          {postBeingEdited && (
-            <div className="max-w-4xl mt-5 bg-gray-50 rounded-2xl overflow-hidden shadow-lg ">
-              {!post?.imgUrl && (
-                <img
-                  alt="pic"
-                  className="w-full h-auto  rounded-t-2xl"
-                  src="https://picsum.photos/500/300"
-                />
-              )}
-              {post?.imgUrl && (
-                <img
-                  alt="user"
-                  className="w-full h-auto  rounded-t-2xl "
-                  src={post?.imgUrl}
-                />
-              )}
-              <div className="px-6 py-4 place-self-start">
-                {post && <ReadingTime text={post.body} />}
-              </div>
-
-              <a href="/topics">
-                <span className="inline-block bg-gray-200  rounded-full px-3 py-1 text-sm font-semibold text-gray-700 hover:bg-gray-700 mr-2 mb-2  hover:text-white">
-                  Return to topics
-                </span>
-              </a>
-              <span
-                onClick={handlePrint}
-                className="mr-2 mb-2  inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-blue-700 hover:border-transparent hover:bg-blue-500 hover:text-white"
-              >
-                Print
-              </span>
-              <LikeButton id={id} initialLikes={post?.likes} />
-              <div className="my-1 space-x-4">
-                <FacebookShareButton
-                  quote={post?.title}
-                  url={window.location.href}
-                  hashtag="#myblog"
-                >
-                  <FaFacebook size={20} />
-                </FacebookShareButton>
-                <TwitterShareButton
-                  title={post?.title}
-                  url={window.location.href}
-                  via="@myblog"
-                >
-                  <FaTwitter size={20} />
-                </TwitterShareButton>
-                <LinkedinShareButton>
-                  <FaLinkedin size={20} />
-                </LinkedinShareButton>
-                <button onClick={copyToClipboard}>
-                  {isCopied ? 'Copied!' : <FaLink />}
-                </button>
-              </div>
-
-              <CommentBox
-                postId={id}
-                onCommentMade={() => setReloadTrigger(!reloadTrigger)}
-              />
-
-              <h3>comments: </h3>
-              {post?.comments?.map((comment) => (
-                <div key={comment._id}>
-                  <p>{comment.body}</p>
-                  {isLoggedIn && (
-                    <button onClick={() => deleteComment(comment._id)}>
-                      Delete
-                    </button>
-                  )}
-                </div>
-              ))}
-              {isLoggedIn && user?._id === post?.author?._id && (
-                <button
-                  onClick={handleEdit}
-                  className="bg-cyan-500 text-white font-medium px-8 py-1 my-1 rounded mt-2"
-                >
-                  {!isEditing ? 'Edit' : 'finish edition'}
-                </button>
-              )}
-              {isEditing && (
-                <div ref={editPostRef}>
-                  <EditPostCard
-                    postBeingEdited={postBeingEdited}
-                    onCancel={cancelEditing}
-                  />
-                </div>
-              )}
-            </div>
-          )}
+    <div
+      ref={componentRef}
+      className="flex justify-start items-center relative bg-gray-200 pt-5 pb-10 font-pop"
+    >
+      <div className="flex flex-col  w-full h-full">
+        <div className="self-start text-2xl font-bold capitalize">
+          <h1>{post?.title}</h1>
         </div>
-<span>#####</span>
-
-              <div
-                dangerouslySetInnerHTML={{ __html: post?.body }}
-                className="post-content"
-              ></div>
+        <div
+          dangerouslySetInnerHTML={{ __html: post?.body }}
+          className="post-content self-start "
+        ></div>
+        {postBeingEdited && (
+          <div className="max-w-4xl mt-5 bg-gray-50 rounded-2xl overflow-hidden shadow-lg ">
+            {!post?.imgUrl && (
+              <img
+                alt="pic"
+                className="w-full h-auto  rounded-t-2xl"
+                src="https://picsum.photos/500/300"
+              />
+            )}
+            {post?.imgUrl && (
+              <img
+                alt="user"
+                className="w-full h-auto  rounded-t-2xl "
+                src={post?.imgUrl}
+              />
+            )}
+            <div className="px-6 py-4 place-self-start">
+              {post && <ReadingTime text={post.body} />}
             </div>
+
             <a href="/topics">
               <span className="inline-block bg-gray-200  rounded-full px-3 py-1 text-sm font-semibold text-gray-700 hover:bg-gray-700 mr-2 mb-2  hover:text-white">
                 Return to topics
@@ -263,8 +185,7 @@ export const SinglePost = () => {
             )}
           </div>
         )}
-
       </div>
-    </>
+    </div>
   );
 };
